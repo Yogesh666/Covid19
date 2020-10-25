e48289f72ffc13cb6fc12b58635653460123614b
44444444444444444444 <44444444444444444444@192.168.1.205>
2020-10-25 09:16:48 +0530

    Changedata

--- src/MyClass.java
+++ src/MyClass.java
@@ -6,11 +6,11 @@ public class MyClass {
 		System.out.println("Hello World Program");
 		String str = "Pune is the City";
 		ArrayList<String> ls = new ArrayList<String>();
-		ls.add("one");
-		ls.add("Two");
-		ls.add("Three");
-		ls.add("Four");
-		ls.add("Five");
+		ls.add("one2");
+		ls.add("Two2");
+		ls.add("Three3");
+		ls.add("Four4");
+		ls.add("Five6");
 		
 		for(String s : ls) {
 			if(s.length() < 4)
