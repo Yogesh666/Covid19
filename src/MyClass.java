fc14e944c1951572901962475536f94697e0080e
44444444444444444444 <44444444444444444444@192.168.1.205>
2020-10-25 08:59:54 +0530

    FirstCommit

---
+++ src/MyClass.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World Program");
+		String str = "Pune is the City";
+		ArrayList<String> ls = new ArrayList<String>();
+		ls.add("one");
+		ls.add("Two");
+		ls.add("Three");
+		ls.add("Four");
+		ls.add("Five");
+		
+		for(String s : ls) {
+			if(s.length() < 4)
+				
+			System.out.println(s);
+		}
+		
+		String[] st = str.split(" ");
+		for(String sp : st)
+		{
+			System.out.println(sp);
+		}
+			
+		
+		
+	}
+
+}
